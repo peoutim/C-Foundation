0f97d8fca69d15e286598be0ef1f58e2274161c8
peoutim <timster@uw.edu>
2016-08-05 16:42:41 -0700

    HomeWork7 - Modify DynamicArray to Insert and RemoveAt element.

---
+++ Peou Tim/HomeWork7/HomeWork7/HomeWork7/DynamicArray.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork7
+{
+    /// <summary>
+    /// Programmer: Tim, Peou
+    /// Homework7: Modify DynArray to be able to insert and delete array elements. 
+    /// </summary>
+    class DynamicArray
+    {
+        /*Dynamic Array - Unlike traditional array, Dynamic Array allow array to grow
+        /Dynamic array data structure has following fields:
+        /--------------------------------------------------
+        /storage: dynamically allocated space to store data;
+        /capacity value: size of the storage;
+        /size value: size of the real data.*/
+
+
+        //Fields
+
+        private int[] storage;
+
+        private int size;
+
+        public DynamicArray()
+        {
+
+            storage = new int[6];
+
+            size = 0;
+        }
+
+        public DynamicArray(int capacity)
+        {
+            storage = new int[capacity];
+            size = 0;
+        }
+
+        //Constructor
+        public DynamicArray(int[] array)
+        {
+            storage = array;
+        }
+
+        //Ensure the Array have the capacity
+        public void ensureCapacity(int minCapacity)
+        {
+
+            int capacity = storage.Length;
+
+            if (minCapacity > capacity)
+            {
+                int newCapacity = (capacity * 3) / 2 + 1;
+
+                if (newCapacity < minCapacity)
+
+                    newCapacity = minCapacity;
+
+                storage.CopyTo(storage, newCapacity);
+            }
+        }
+
+        //Display Method
+        public void Display(int[] array)
+        {
+            foreach (int x in array)
+            {
+                Console.Write("{0,2} ", x);
+            }
+            Console.WriteLine();
+        }
+
+        //Insert Method at Index Location
+        public void Insert(int index, int val)
+        {
+            
+            ensureCapacity(size + 1);
+
+            int moveCount = size - index;
+
+            if (moveCount > 0)
+
+                Array.Copy(storage, index, storage, index + 1, moveCount);
+
+            storage[index] = val;
+        }
+
+        //Method to remove at Index Location
+        public void RemoveAt(int index)
+        {
+           
+            int moveCount = size - index - 1;
+
+            if (moveCount > 0)
+
+                Array.Copy(storage, index, storage, index + 1, moveCount);
+
+            storage[index] = -1;
+        }
+    }//End Class
+}
---
+++ Peou Tim/HomeWork7/HomeWork7/HomeWork7/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork7
+{
+    /// <summary>
+    /// Programmer: Tim, Peou
+    /// Homework7: Modify DynArray to be able to insert and delete array elements. 
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[6] { 10, 20, 30, 40, 50, -1 };
+
+            //Create an instance of DynamicArray Object
+
+            DynamicArray da = new DynamicArray(array);
+
+            //Displaying, inserting an element, and removing an Element 
+
+            Console.WriteLine("Initial Display:");
+            da.Display(array);
+
+            Console.WriteLine("Display - After Insertion:");
+            da.Insert(2, 22);
+            da.Display(array);
+
+            Console.WriteLine("Display - After Removal:");
+            da.RemoveAt(3);
+            da.Display(array);
+
+            Console.WriteLine();   
+            Console.Write("Press Enter...");
+            Console.ReadLine();
+        }//End Main
+    }
+}
